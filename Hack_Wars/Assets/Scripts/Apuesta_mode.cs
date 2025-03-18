5a6c6b8ffead0f6832fb4aa4a6e5da9e9ca06068
SergioRoyo <SERGIOROYOESCA@GMAIL.COM>
2025-03-18 18:12:01 +0100

    apuesta ruleta

--- Hack_Wars/Assets/Scripts/Apuesta_mode.cs
+++ Hack_Wars/Assets/Scripts/Apuesta_mode.cs
@@ -4,21 +4,94 @@ using UnityEngine;
 
 public class Apuesta_mode : MonoBehaviour
 {
-    public bool changeApuesta;
+    public bool changeApuesta = false;
+    public bool apuestaHecha = false;
+
+    public bool rojoApuesta = false;
+    public bool negroApuesta = false;
+    public bool verdeApuesta = false;
+
+    public bool rojoRuleta = false;
+    public bool negroRuleta = false;
+    public bool verdeRuleta = false;
+
+    public int timeChange = 30;
+    public int probabilidad;
+    public int dañoABoss;
+
+    public GameObject botonApuesta;
+    public GameObject zonaApuesta;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(TimeApuesta());
+        zonaApuesta.SetActive(false);
+        botonApuesta.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (apuestaHecha)
+        {
+            StartCoroutine(TimeApuesta());
+            zonaApuesta.SetActive(false);
+        }
+
+        if (probabilidad <= 4) //verde
+        {
+            verdeRuleta = true;
+            ResultadoApuesta();
+
+        }
+        else if (probabilidad >= 5 || probabilidad <= 48) // rojo
+        {
+            rojoRuleta = true;
+            ResultadoApuesta();
+        }
+        else // negro
+        { 
+            negroRuleta = true;
+            ResultadoApuesta();
+        }
+
+
     }
 
-    void Apuesta()
+    public void Apuesta()
     {
+        zonaApuesta.SetActive(true);
+        botonApuesta.SetActive(false);
 
     }
+    public void ResultadoApuesta()
+    {
+        if (rojoApuesta || rojoRuleta) 
+        {
+            //Dano a boss
+        }
+        else if (negroApuesta || negroRuleta)
+        {
+            //Dano a boss
+        }
+        else if (verdeApuesta || verdeRuleta)
+        {
+
+            //Destroy(Boss);
+        }
+        else
+        {
+            //Dano a player
+        }
+
+    }
+
+    IEnumerator TimeApuesta()
+    {
+        yield return new WaitForSeconds(timeChange);
+        botonApuesta.SetActive(true);
+    }
+
+
 }
