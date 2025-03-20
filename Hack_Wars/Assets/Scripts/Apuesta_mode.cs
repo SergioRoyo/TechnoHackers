6f4a2e43139c03351169410986e2d316916568a8
SergioRoyo <SERGIOROYOESCA@GMAIL.COM>
2025-03-20 19:58:53 +0100

    probabilidad apuestas y movimiento de bossbola

--- Hack_Wars/Assets/Scripts/Apuesta_mode.cs
+++ Hack_Wars/Assets/Scripts/Apuesta_mode.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Apuesta_mode : MonoBehaviour
@@ -15,9 +16,9 @@ public class Apuesta_mode : MonoBehaviour
     public bool negroRuleta = false;
     public bool verdeRuleta = false;
 
-    public int timeChange = 30;
-    public int probabilidad;
-    public int dañoABoss;
+    public int timeChange = 30; // tiempo cada que se activa el modo apuesta
+    public int probabilidad; // numero del 1 al 100 que se usa para hacer la probabilidad de la ruleta
+    public int dañoABoss; // cantidad de daño que se le hace al boss si ganas en la ruleta
 
     public GameObject botonApuesta;
     public GameObject zonaApuesta;
@@ -33,33 +34,40 @@ public class Apuesta_mode : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (apuestaHecha)
+        if (apuestaHecha) // Una vez hecha la apuesta se vuelve a activar el tiempo para el modo apuesta
         {
             StartCoroutine(TimeApuesta());
             zonaApuesta.SetActive(false);
         }
 
-        if (probabilidad <= 4) //verde
+        if (probabilidad <= 4) //despues de apostar, probalbilidad de que te toque verde
         {
             verdeRuleta = true;
             ResultadoApuesta();
 
         }
-        else if (probabilidad >= 5 || probabilidad <= 48) // rojo
+        else if (probabilidad >= 5 || probabilidad <= 48) // despues de apostar, probalbilidad de que te toque rojo
         {
             rojoRuleta = true;
             ResultadoApuesta();
         }
-        else // negro
-        { 
+        else //despues de apostar, probalbilidad de que te toque negro
+        {
             negroRuleta = true;
             ResultadoApuesta();
         }
 
 
+        //probabilidad = Random.Range(0,100) ; codigo a añadir al darle a apostar
+        // y añadir uno de estos el que sea en cada caso
+        //rojoApuesta = true;
+        //negroApuesta = true;
+        //verdeApuesta = true;
+
+
     }
 
-    public void Apuesta()
+    public void Apuesta() //funcion llamada por el boton de apostar para que se active la zona de apuesta y el boton de apostar se desactive 
     {
         zonaApuesta.SetActive(true);
         botonApuesta.SetActive(false);
@@ -67,7 +75,8 @@ public class Apuesta_mode : MonoBehaviour
     }
     public void ResultadoApuesta()
     {
-        if (rojoApuesta || rojoRuleta) 
+
+        if (rojoApuesta || rojoRuleta)
         {
             //Dano a boss
         }
@@ -75,19 +84,26 @@ public class Apuesta_mode : MonoBehaviour
         {
             //Dano a boss
         }
-        else if (verdeApuesta || verdeRuleta)
+        else if (verdeApuesta || verdeRuleta)// al ser tan baja la probabilidad de que te toque el verde, si apuestas y te toca, matas automaticamente al boss
         {
 
-            //Destroy(Boss);
+            //Destruir a (Boss) ;
         }
         else
         {
-            //Dano a player
+            //Dano a player, te quita una vida
         }
+            rojoApuesta = false;
+            negroApuesta = false;
+            verdeApuesta = false;
+
+            rojoRuleta = false;
+            negroRuleta = false;
+            verdeRuleta = false;
 
     }
 
-    IEnumerator TimeApuesta()
+    IEnumerator TimeApuesta() // cada (timeChange) segundos se activa el modo apuesta en la partida
     {
         yield return new WaitForSeconds(timeChange);
         botonApuesta.SetActive(true);
