b62a9fa4663c2f76565c43b395775e07e034e6f8
SergioRoyo <SERGIOROYOESCA@GMAIL.COM>
2025-04-03 20:49:34 +0200

    talacion de grass(grasa pura)

--- Xtra_Xaotic_Xroll/Assets/SCRIPTS/GRASS_SPAWNER.cs
+++ Xtra_Xaotic_Xroll/Assets/SCRIPTS/GRASS_SPAWNER.cs
@@ -6,9 +6,7 @@ public class GRASS_SPAWNER : MonoBehaviour
 {
     public GameObject grassSpawner;
     public int randomSpawn;
-    public GameObject drop2;
-    public bool destroyGrassCoolDown;
-    public int grassHealth = 3;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -16,13 +14,13 @@ public class GRASS_SPAWNER : MonoBehaviour
         {
             grassSpawner.transform.GetChild(i).gameObject.SetActive(false);
         }
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.S)) 
+        if (Input.GetKeyUp(KeyCode.S))
         {
             for (int i = 0; i < grassSpawner.transform.childCount; i++)
             {
@@ -30,33 +28,16 @@ public class GRASS_SPAWNER : MonoBehaviour
             }
             SpawnRandomGrass();
         }
-        if(grassHealth <= 0)
-        {
-            GenerateDrop();
-            Destroy(this.gameObject);  
-        }
+       
     }
     public void SpawnRandomGrass()
     {
-        int numOfGrass = Random.Range(grassSpawner.transform.childCount/4, grassSpawner.transform.childCount/2);
+        int numOfGrass = Random.Range(grassSpawner.transform.childCount / 4, grassSpawner.transform.childCount / 2);
         for (int i = 0; i < numOfGrass; i++)
         {
             grassSpawner.transform.GetChild(Random.Range(0, grassSpawner.transform.childCount)).gameObject.SetActive(true);
         }
     }
-    public void GenerateDrop()
-    {
-        
-        Instantiate(drop2, transform.position, Quaternion.identity);
-    }
-    public IEnumerator GrassDestroy()
-    {
-        if (!destroyGrassCoolDown)
-        {
-            destroyGrassCoolDown = true;
-            grassHealth--;
-            yield return new WaitForSeconds(.2f);
-            destroyGrassCoolDown = false;
-        }
-    }
+    
+    
 }
