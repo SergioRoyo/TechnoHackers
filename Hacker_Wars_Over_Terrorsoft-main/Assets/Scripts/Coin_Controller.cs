1c7a9dfa05352de55b0b6681f394e02e0c2ec2b9
SergioRoyo <SERGIOROYOESCA@GMAIL.COM>
2025-05-12 18:15:48 +0200

    moneda script

---
+++ Hacker_Wars_Over_Terrorsoft-main/Assets/Scripts/Coin_Controller.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Coin_Controller : MonoBehaviour
+{
+    public int coinCount = 0; // Contador global de monedas
+    public Animator _animator;
+    public TextMeshProUGUI coinText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+            coinText.text = "Monedas: " + coinCount.ToString();
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _animator.SetTrigger("jump");// Activa la animacion 
+            coinCount++; // Aumenta el contador
+            Destroy(gameObject, 2f); // Destruye la moneda
+        }
+    }
+}
